234c1ca3351b747ef93690f17620a5c88abb1438
princechouhan001 <75777425+princechouhan001@users.noreply.github.com>
2024-10-29 00:12:26 +0000

    datatypes

---
+++ 1/02_datatypes.js
@@ -0,0 +1,25 @@
+// Premitive data tyeps
+
+// number 2 to power 53 
+// bigint
+// string => "", '', ``
+// boolean => true, false
+// null => stand alone value
+// undefined => without any value
+// symbol
+
+
+// Object
+
+let str = "letters";
+let num = 523;
+let bool = true;
+let nll = null;
+let undfnd;
+
+
+console.log(typeof "letters");
+console.log(typeof 532);
+console.log(typeof true);
+console.log(typeof undfnd);
+console.log(typeof null);
