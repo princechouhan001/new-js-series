8fb581fd158bfb73a5ab56b702a6ffec6eb6ec85
princechouhan001 <75777425+princechouhan001@users.noreply.github.com>
2024-10-29 00:32:43 +0000

    Conversion of Data Types

---
+++ 1/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "53abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score1 = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber1 = Number(score)
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// "string" => true
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 123
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
